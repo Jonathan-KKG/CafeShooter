a67f1dbeded5ca9ec1bfd6acd42d124ac36900ab
Habibelibu <habib_2006dortmund@outlook.de>
2023-12-04 18:47:38 +0100

    Added Cook & Shooter classes, Code Clean-Up

---
+++ src/main/java/my_project/model/Cook.java
@@ -0,0 +1,9 @@
+package my_project.model;
+
+import KAGO_framework.control.ViewController;
+
+public class Cook extends Player{
+    public Cook(double pX, double pY, ViewController pViewController) {
+        super(pX, pY, pViewController);
+    }
+}
---
+++ src/main/java/my_project/model/Shooter.java
@@ -0,0 +1,9 @@
+package my_project.model;
+
+import KAGO_framework.control.ViewController;
+
+public class Shooter extends Player{
+    public Shooter(double pX, double pY, ViewController pViewController) {
+        super(pX, pY, pViewController);
+    }
+}

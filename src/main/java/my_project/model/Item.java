4eaeb7d6c4b3d489e38597fa67210ed3d43ec5b6
Habibelibu <habib_2006dortmund@outlook.de>
2023-12-19 20:09:41 +0100

    Added second constructor in Item to fix merge issue

--- src/main/java/my_project/model/Item.java
+++ src/main/java/my_project/model/Item.java
@@ -4,12 +4,24 @@ import KAGO_framework.view.DrawTool;
 
 public abstract class Item extends Entity {
     private String type;
+
     /**
      * @param filename the Immage that should be Drawn.
      * @param pX       the start x-Position
      * @param pY       the stard y-Position
      */
-    public Item(String filename, double pX, double pY,String pType) {
+    public Item(String filename, double pX, double pY) {
+        super(filename, pX, pY);
+        type = filename;
+    }
+
+    /**
+     * This Constructor should be used for dishes as they are in a subfolder.
+     * @param filename the Immage that should be Drawn.
+     * @param pX       the start x-Position
+     * @param pY       the stard y-Position
+     */
+    public Item(String filename, double pX, double pY, String pType) {
         super(filename, pX, pY);
         type = pType;
     }
